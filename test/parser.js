7fafab1e215eec35136505b3380ed5a51c5ab578
Julian Jensen <jjdanois@gmail.com>
2018-04-07 09:59:27 -0400

    Back from NYC

--- test/parser.js
+++ test/parser.js
@@ -1,48 +1,52 @@
 /* eslint-env mocha */
 'use strict';
 
-var _ = require( 'lodash' );
-var Ajv = require( 'ajv' );
-var fs = require( 'fs' );
-var helper = require( './helper' );
-var parse = require( '../lib/parser' ).parse;
-var path = require( 'path' );
-var schema = require( '../lib/schema' );
-var should = require( 'should' );
-var equal = should.equal;
-var util = require( 'util' );
-var fixtures = require( './fixtures/externals.json' );
+var _ = require('lodash');
+var Ajv = require('ajv');
+var fs = require('fs');
+var helper = require('./helper');
+var parse = require('../lib/parser').parse;
+var path = require('path');
+var schema = require('../lib/schema');
+var should = require('should');
+var util = require('util');
+var fixtures = require('./fixtures/externals.json');
 // var fixtures =  [ 'catharsis', 'closure-library', 'jsdoc', 'jsduck', 'ts' ]
 //     .map( fn => ( { name: fn, data: fs.readFileSync( path.join( './test/fixtures', fn + '-types' ), 'utf8' ) } ) );
+util.inspect.defaultOptions = { depth: 10, colors: false, showHidden: true };
 
-var ajv = new Ajv( {
+
+var ajv = new Ajv({
     allErrors:     true,
     ownProperties: true
-} );
-var validate = ajv.compile( schema ),
-    isObject = o => typeof o === 'object' && o !== null && !Array.isArray( o );
+});
+var validate = ajv.compile(schema),
+    isObject = o => typeof o === 'object' && o !== null && !Array.isArray(o);
 
-function remove_nullable( obj )
-{
-    if ( Array.isArray( obj ) ) {
-        return obj.map( remove_nullable );
+function remove_nullable(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(remove_nullable);
     }
 
-    if ( !isObject( obj ) ) {
+    if (!isObject(obj)) {
         return obj;
     }
 
-    if ( obj.nullable === false ) {
+    if (obj.nullable === false) {
         delete obj.nullable;
     }
 
-    let no = {};
+    let no    = {},
+        descs = Object.getOwnPropertyDescriptors(obj);
 
-    Object.keys( obj ).forEach( key => {
-        if ( obj[key] !== null ) {
-            no[key] = remove_nullable( obj[ key ] );
+    Object.keys(descs).forEach(key => {
+        if (obj[key] !== null) {
+            if (descs[key].enumerable)
+                no[key] = remove_nullable(obj[key]);
+            else
+                Object.defineProperty(no, key, { enumerable: false, value: remove_nullable(obj[key]) });
         }
-    } );
+    });
 
     return no;
 }
@@ -52,20 +56,17 @@ function remove_nullable( obj )
  * @param {object} options
  * @return {*}
  */
-function parseIt( item, options )
-{
+function parseIt(item, options) {
     var parsed;
 
-    try
-    {
+    try {
         // console.log( `parseIt( "${item.newExpression}", ${JSON.stringify( options )} )` );
-        parsed = parse( item.newExpression || item.expression, options );
+        parsed = parse(item.newExpression || item.expression, options);
         // console.log( `DONE:`, parsed );
     }
-    catch ( e )
-    {
-        throw new Error( util.format( 'unable to parse type expression "%s": %s', item.expression,
-            e.message ) );
+    catch (e) {
+        throw new Error(util.format('unable to parse type expression "%s": %s', item.expression,
+            e.message));
     }
 
     // if (!_.isEqual(parsed, item.parsed)) {
@@ -75,101 +76,94 @@ function parseIt( item, options )
     return parsed;
 }
 
-function checkTypes( filepath, options )
-{
-    var types = require( filepath );
+function checkTypes(filepath, options) {
+    var types = require(filepath);
 
     var errors = [];
     var parsedType;
     var validationErrors = [];
     var validationResult;
 
-    types.forEach( function( type ) {
-        try
-        {
-            parsedType = parseIt( type, options );
-            validationResult = validate( parsedType );
-            if ( validationResult === false )
-            {
-                validationErrors.push( {
+    types.forEach(function(type) {
+        try {
+            parsedType = parseIt(type, options);
+            validationResult = validate(parsedType);
+            if (validationResult === false) {
+                validationErrors.push({
                     expression: type.expression,
-                    errors:     validate.errors.slice( 0 )
-                } );
+                    errors:     validate.errors.slice(0)
+                });
             }
         }
-        catch ( e )
-        {
-            errors.push( e.message );
+        catch (e) {
+            errors.push(e.message);
         }
-    } );
+    });
 
-    errors.should.eql( [] );
-    validationErrors.should.eql( [] );
+    errors.should.eql([]);
+    validationErrors.should.eql([]);
 }
 
-describe( 'parser', function() {
-    describe( 'parse()', function() {
+describe('parser', function() {
+    describe('parse()', function() {
         var specs = './test/specs';
-        var jsdocSpecs = path.join( specs, 'jsdoc' );
+        var jsdocSpecs = path.join(specs, 'jsdoc');
 
-        function tester( specPath, basename )
-        {
-            it( 'can parse types in the "' + basename + '" spec', function() {
-                checkTypes( path.join( specPath, basename ), { jsdoc: true } );
-            } );
+        function tester(specPath, basename) {
+            it('can parse types in the "' + basename + '" spec', function() {
+                checkTypes(path.join(specPath, basename), { jsdoc: true });
+            });
         }
 
-        function jsdocTester( specPath, basename )
-        {
-            it( 'can parse types in the "' + basename + '" spec when JSDoc type parsing is enabled',
+        function jsdocTester(specPath, basename) {
+            it('can parse types in the "' + basename + '" spec when JSDoc type parsing is enabled',
                 function() {
-                    checkTypes( path.join( specPath, basename ), { jsdoc: true } );
-                } );
+                    checkTypes(path.join(specPath, basename), { jsdoc: true });
+                });
         }
 
-        helper.testSpecs( specs, tester );
-        helper.testSpecs( jsdocSpecs, jsdocTester );
-    } );
+        helper.testSpecs(specs, tester);
+        helper.testSpecs(jsdocSpecs, jsdocTester);
+    });
 
-    describe( 'perse() external types', function() {
+    describe('perse() external types', function() {
 
-        function test_block( testData )
-        {
+        function test_block(testData) {
             let testCount = 0,
                 errors    = [];
 
-            it( 'can parse types from the ' + testData[ 0 ].system + ' list', function() {
+            it('can parse types from the ' + testData[0].system + ' list', function() {
                 let errorInfo;
 
-                testData.forEach( ( { expr: expression, ast } ) => {
-                    try
-                    {
+                testData.forEach(({ expr: expression, ast }) => {
+                    try {
                         errorInfo = null;
                         // console.log( `parsing ${expression}` );
-                        ast = remove_nullable( ast );
-                        let parsed = parseIt( { newExpression: expression }, { jsdoc: true } );
+                        ast = remove_nullable(ast);
+                        let parsed = parseIt({ newExpression: expression }, { jsdoc: true });
                         // console.log( 'parsed:', parsed );
                         testCount++;
-                        parsed = remove_nullable( parsed );
+                        parsed = remove_nullable(parsed);
                         errorInfo = { ast, parsed, expression };
-                        should( ast ).eql( parsed );
+                        should(ast).eql(parsed);
                     }
-                    catch ( err )
-                    {
-                        console.log( `ERROR: "${expression}"` );
-                        if ( errorInfo )
-                        {
-                            console.log( '   ast:\n', errorInfo.ast );
-                            console.log( 'parsed:\n', errorInfo.parsed );
+                    catch (err) {
+                        console.log(`ERROR: "${expression}"`);
+                        if (errorInfo) {
+                            console.log('   ast:\n', util.inspect(errorInfo.ast, { depth: 10, colors: false, showHidden: true }));
+                            console.log('parsed:\n', util.inspect(errorInfo.parsed, { depth: 10, colors: false, showHidden: true }));
                         }
-                        // console.log( err );
-                        console.log( '\n' );
-                        errors.push( err );
-                    }
-                } );
+                        else
+                            console.log('   ast:\n', util.inspect(ast, { depth: 10, colors: false, showHidden: true }));
 
-                console.log( `name: ${testData[ 0 ].system}, count: ${testCount}, errors: ${errors.length}` );
-            } );
+                        // console.log( err );
+                        console.log('\n');
+                        errors.push(err);
+                    }
+                });
+
+                console.log(`name: ${testData[0].system}, count: ${testCount}, errors: ${errors.length}`);
+            });
 
             return errors;
         }
@@ -178,26 +172,25 @@ describe( 'parser', function() {
             sysTypes = new Set(),
             grouped  = {};
 
-        fixtures.forEach( item => {
-            if ( !grouped[ item.system ] )
-            {
-                grouped[ item.system ] = [];
+        fixtures.forEach(item => {
+            if (!grouped[item.system]) {
+                grouped[item.system] = [];
             }
 
-            sysTypes.add( item.system );
-            grouped[ item.system ].push( item );
-        } );
+            sysTypes.add(item.system);
+            grouped[item.system].push(item);
+        });
 
 
-        sysTypes.forEach( function( sysName ) {
-            const testData = grouped[ sysName ];
+        sysTypes.forEach(function(sysName) {
+            const testData = grouped[sysName];
 
-            console.log( 'running it on ' + sysName + ', data size:', testData.length );
+            console.log('running it on ' + sysName + ', data size:', testData.length);
 
-            const e = test_block( testData );
+            const e = test_block(testData);
 
-            e.should.have.property( 'length', 0 );
-        } );
+            e.should.have.property('length', 0);
+        });
 
-    } );
-} );
+    });
+});
